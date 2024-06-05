f9cf75edfe72d0161dfc52781cf650c2938df420
BakhtawarAbbasi <bakhtawarabbasi009@gmail.com>
2024-06-05 22:14:38 +0500

    Word-counter

--- index.js
+++ index.js
@@ -1,30 +1,25 @@
-function countWords(text: string): { [word: string]: number}{
+function countWords(text) {
     //Convert the text to lowercase to ensure case-insensitive counting
     const lowerCaseText = text.toLocaleLowerCase();
-
     //Use a regular expression to splits the text into words
     const word = lowerCaseText.match(/\b\w+\b/g);
-
     //Initialize an empty object to store word counts
-    const wordCounts: { [word: string]: number} = {};
-    
+    const wordCounts = {};
     //Iterate over each word in the words array
-    if (word){
+    if (word) {
         word.forEach(word => {
-            if(wordCounts[word]){
+            if (wordCounts[word]) {
                 wordCounts[word]++;
             }
-            else{
+            else {
                 wordCounts[word] = 1;
             }
         });
     }
-
     //Return the object containing word counts
     return wordCounts;
-
 }
-
 const text = "Hello World! Hello Typescript";
 const wordCounts = countWords(text);
 console.log(wordCounts);
+export {};
